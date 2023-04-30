28c02cd9fa8f2dacf9d239801b98141a2feaae91
Nicole Slater <nicoleslater@pursuit.org>
2023-04-30 18:14:58 -0400

    Updating Repository

---
+++ MODULE_1/javascript-on-your-machine/fruits.js
@@ -0,0 +1,14 @@
+function addFruit(fruits, fruit) {
+    fruits.push(fruit);
+    return fruits;
+  }
+  
+  const fruits = ["🍓", "🍐", "🍎"];
+  const input = process.argv[2];
+  if (input) {
+    let result = addFruit(fruits, input);
+    console.log(result);
+  } else {
+    console.log("No fruit added...");
+    console.log(input)
+  }
