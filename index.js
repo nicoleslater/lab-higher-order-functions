2ae8b84ec12529fb493eea2d6185987bbb88839a
Nicole Slater <nicoleslater@pursuit.org>
2023-04-30 14:12:02 -0400

    11 Tests Passed

--- index.js
+++ index.js
@@ -14,12 +14,13 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    callback(element) => {
-      
-    }
-    
+  if (callback(element)) {
+    return element;
   }
+  }
+  return undefined;
 }
+
 
 /**
  * Returns an array of all elements in the array that cause the callback to return `true`. If the array is empty or no elements cause the callback to return `true`, then return an empty array.
@@ -38,10 +39,13 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+   if (callback(element)) {
+    result.push(element);
+   }
   }
   return result;
 }
+
 
 /**
  * Returns an array where each element is transformed by the callback. If the array is empty, return an empty array.
@@ -60,7 +64,9 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)){
+      result.push(callback)
+    }
   }
   return result;
 }
